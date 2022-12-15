1c0a7e92b2f1b43d2a364b095034b934f33bf90d
tdevere <tdevere@microsoft.com>
2022-12-15 11:43:37 -0600

    Add project files.

---
+++ UITest/AppInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .ApkFile(@"C:\UITEST\com.companyname.appcenterxamrain_test_example.apk")
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ UITest/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        private void StepWork()
+        {
+            System.Threading.Thread.Sleep(1000);
+            app.Tap(c => c.Marked("btnMyButton"));
+            app.Screenshot("LargeStep");
+        }
+    }
+}
